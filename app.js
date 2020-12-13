edfb1c1736ad201a711bd0fc746968e727e56f70
kartikey110813@gmail.com <kartikey110813@gmail.com>
2020-12-13 14:14:27 +0530

    message

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
 // Side Menu
-const sideNav = document.querySelector('.sidenav');
+const sideNav = document.querySelector(".sidenav");
 M.Sidenav.init(sideNav, {});
 
 // Slider
-const slider = document.querySelector('.slider');
+const slider = document.querySelector(".slider");
 M.Slider.init(slider, {
-    indicators: false,
-    height: 500,
-    transition: 500,
-    interval: 2000
-  });
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 2000,
+});
 
 // Scrollspy
-const ss = document.querySelectorAll('.scrollspy');
+const ss = document.querySelectorAll(".scrollspy");
 M.ScrollSpy.init(ss, {});
 
 // Material Boxed
-const mb = document.querySelectorAll('.materialboxed');
+const mb = document.querySelectorAll(".materialboxed");
 M.Materialbox.init(mb, {});
 
 // Auto Complete
-const ac = document.querySelector('.autocomplete');
+const ac = document.querySelector(".autocomplete");
 M.Autocomplete.init(ac, {
-    data: {
-      "Mysore palace": null,
-      "Brindavan garden": null,
-      "Sri chamarajendra zoological garden": null,
-      "sri chamundeshwari temple": null,
-      "California": null,
-      "Jamaica": null,
-      "Europe": null,
-      "The Bahamas": null,
-    }
-  });
- 
+  data: {
+    "Mysore palace": null,
+    "Brindavan garden": null,
+    "Sri chamarajendra zoological garden": null,
+    "sri chamundeshwari temple": null,
+    California: null,
+    Jamaica: null,
+    Europe: null,
+    "The Bahamas": null,
+  },
+});
+
+function done() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const message = document.getElementById("message").value;
+  console.log(name);
+
+  alert(
+    "Thanks For Your Response we will get back to you soon! \r\n\r\nName  : " +
+      name +
+      "\r\nEmail :  " +
+      email +
+      "\r\nPhone : " +
+      phone +
+      "\r\nMessage : " +
+      message +
+      "\r\n\r\n For more Queries go to @mysuru Instagram"
+  );
+}
