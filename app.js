2b496e01b2e7dc2bcc769718892ebdfab0145da2
Vipul <dowhile03@gmail.com>
2020-12-13 11:03:36 +0530

    vipul commit

--- app.js
+++ app.js
@@ -33,19 +33,4 @@ M.Autocomplete.init(ac, {
       "The Bahamas": null,
     }
   });
-  function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("autocomplete-input");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+ 
